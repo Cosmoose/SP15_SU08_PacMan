8ceec15c2a6209d6801e0333ef9ef4ea49903bd1
Johan Anderstedt <jumpi_inn@hotmail.com>
2015-11-12 16:08:52 +0100

    Objects
    Gjort så att pacman kan dödas och respawna, har liv och fått in
    CollideDestroy scriptet i Objects scriptet.
    Se till att ta bort alla CollideDestroy script från spökerna.
    Se till att Respawn Prefab i scriptet är pacman och att Respawn i
    scriptet är gameobjektet som definerar pacmans respawn punkt.

--- Assets/Pacman/Scripts/Objects.cs
+++ Assets/Pacman/Scripts/Objects.cs
@@ -3,10 +3,31 @@ using System.Collections;
 
 public class Objects : MonoBehaviour {
 	int score = 0;
-	void Start () {
-		
+	public int lives = 3;
+	public GameObject respawnPrefab;
+	public GameObject respawn;
+	void Start() {
+		if (respawn == null)
+			respawn = GameObject.FindWithTag("Respawn");
 	}
+
 	void OnCollisionEnter (Collision col)
+	{
+		if((col.gameObject.tag == "ghost")&&(gameObject.tag == "hunting"))
+		{
+			score += 200;
+			Destroy(col.gameObject);
+		}
+		if ((col.gameObject.tag == "ghost")&&(gameObject.tag == "Untagged")){
+			lives-= 1;
+			Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+			Destroy(gameObject);
+		}
+		if (lives == 0){
+			Destroy(gameObject);
+		}
+	}
+	void OnTriggerEnter(Collider col)
 	{
 		if(col.gameObject.tag == "Power UP")
 		{
@@ -24,9 +45,17 @@ public class Objects : MonoBehaviour {
 			Destroy(col.gameObject);
 			score += 300;
 		}
+
 	}
+
 	void OnGUI()
 	{
-		GUI.Label (new Rect (10, 10, 1000, 30), "Your score is: " +score);
+		GUI.Label (new Rect (10, 10, 1000, 30), "Your score is: " +score +" points");
+		if (lives != 1){
+			GUI.Label (new Rect (10, 30, 1000, 30), "You have " +lives +" lives left");
+		}
+		else if (lives == 1){
+			GUI.Label (new Rect (10, 30, 1000, 30), "You have " +lives +" live left!");
+		}
 	}
 }
