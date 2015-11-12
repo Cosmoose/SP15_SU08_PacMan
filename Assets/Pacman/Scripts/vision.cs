448e0b4ce716462e809bc5aa0f718ea8ff678ee2
Mrzabai <joakim.engholm@gmail.com>
2015-11-12 19:55:46 +0100

    Byggt maze_ lagt in Lajka_förstorat hela banan_lagt in vattentanks och växter
    Det  som står ovanför

---
+++ Assets/Pacman/Scripts/vision.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vision : MonoBehaviour {
+	
+	public ghostMovement ghostMovement;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.name == "pacman")
+		{
+			Debug.Log("enters");
+			ghostMovement.spotted = true;
+
+			if (other.gameObject.tag == "hunting")
+			{
+				ghostMovement.hunted = true;
+			}
+		} 
+		
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.name == "pacman")
+		{
+			Debug.Log("exits");
+			ghostMovement.spotted = false;
+
+			if (other.gameObject.tag == "hunting")
+			{
+				ghostMovement.hunted = false;
+			}
+		}
+		
+	}
+}
